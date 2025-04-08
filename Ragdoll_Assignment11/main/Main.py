00edd807f4e2c7d42ae05a718502d97e355835d1
Peyton Bock <bockps@mail.uc.edu>
2025-04-08 13:00:51 -0400

    Add project files.

---
+++ Ragdoll_Assignment11/main/Main.py
@@ -0,0 +1,27 @@
+# File Name : main.py
+# Student Name: Peyton Bock
+# email:  bockps@mail.uc.edu
+# Assignment Number: Assignment 11  
+# Due Date:   4/17/25
+# Course #/Section:   4010-001
+# Semester/Year:   Spring 2025
+# Brief Description of the assignment:  Clean up CSV data
+# Brief Description of what this module does: This module just contains this single assignment
+#Citations: 
+
+# Anything else that's relevant: N/A
+
+if __name__ == "__main__":
+    pass
+
+
+    #Peter Work Area
+
+
+
+    #Peyton Work Area
+
+
+
+    #End if statement
+
---
+++ Ragdoll_Assignment11/peterWorkPackage/peterWork.py
@@ -0,0 +1,13 @@
+# File Name : peterWork.py
+# Student Name: 
+# email:  
+# Assignment Number: Assignment 11  
+# Due Date:   4/17/25
+# Course #/Section:   4010-001
+# Semester/Year:   Spring 2025
+# Brief Description of the assignment:  Clean up CSV data
+# Brief Description of what this module does: This module just contains this single assignment
+#Citations: 
+
+# Anything else that's relevant: N/A
+
